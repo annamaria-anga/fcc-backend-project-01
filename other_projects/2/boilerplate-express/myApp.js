893deb8faa6fab8983d893b58e7f78182ac079fb
Annamaria Anga <annamaria.anga.dev@gmail.com>
2023-12-10 12:54:26 +0100

    06/12

--- other_projects/2/boilerplate-express/myApp.js
+++ other_projects/2/boilerplate-express/myApp.js
@@ -14,9 +14,9 @@ app.use("/public", express.static(absolutePath2));
 
 app.get("/json", (req, response) => {
   if (process.env.MESSAGE_STYLE === "uppercase") {
-    response.send("Hello json".toUpperCase());
+    response.send({ message: "Hello json".toUpperCase() });
   } else {
-    response.send("Hello json");
+    response.send({ message: "Hello json" });
   }
 });
 
